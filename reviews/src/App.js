2373b5db0ab7e11c29ab8eda2c64287ff8645452
abhiramtiyu <abhi.dilu@gmail.com>
2021-08-08 17:17:31 +0530

    new changes
    new changes

---
+++ reviews/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Review from './Review';
+
+function App() {
+  return (
+    <main>
+      <section className="container">
+        <div className="title">
+          <h2>our review</h2>
+          <div className="underline"></div>
+          <Review />
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default App;
